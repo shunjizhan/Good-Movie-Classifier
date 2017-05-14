4ea78bb1f95741c90349fb4bdca57702140ce960
shunji zhan <shunji.zhan@procore.com>
2017-05-14 09:45:21 -0700

    seems to work

--- GoodMovie.py
+++ GoodMovie.py
@@ -8,18 +8,31 @@ def readFile(filename):
 
 
 trainingData = readFile(sys.argv[1])[1:]
+testingData = readFile(sys.argv[2])[1:]
 features = []
+features_test = []
 values = []
+values_test = []
 
 for one in trainingData:
-  print one
-  # features.append(one[:-1])
-  # values.append[one[-1:]]
+  # print one
+  features.append(one[:-1])
+  values.append(one[-1])
 
-testingData = readFile(sys.argv[2])
+for one in testingData:
+  # print one
+  features_test.append(one[:-1])
+  values_test.append(one[-1])
+
+
+
 
 clf = DecisionTreeClassifier(random_state=0)
-# clf.fit(features, values)
+clf.fit(features, values)
 
-# print features
-# print values
+prediction = clf.predict(features_test)
+print list(map(int, prediction))
+print list(map(int, values_test))
+# print features_test
+# print " "
+# print values_test
