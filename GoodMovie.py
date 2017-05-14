afcb8083df6af1e764180fc1b2465001ddd2af8b
shunji zhan <shunji.zhan@procore.com>
2017-05-14 09:34:34 -0700

    parsed training data to only features

--- GoodMovie.py
+++ GoodMovie.py
@@ -1,11 +1,25 @@
 import sys
-import math
+from sklearn.tree import DecisionTreeClassifier
 
 def readFile(filename):
-  lines = [line.rstrip('\n').rstrip('\r') for line in open(filename)]
+  lines = [line.rstrip('\n').rstrip('\r').split(' ')[1:] for line in open(filename)]
   return lines
 
+
+
 trainingData = readFile(sys.argv[1])[1:]
+features = []
+values = []
+
+for one in trainingData:
+  print one
+  # features.append(one[:-1])
+  # values.append[one[-1:]]
+
 testingData = readFile(sys.argv[2])
 
-print trainingData
+clf = DecisionTreeClassifier(random_state=0)
+# clf.fit(features, values)
+
+# print features
+# print values
